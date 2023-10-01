1d6668e7d937c44625c7f3d623af5b25ad7905b5
hmanne23 <143413901+hmanne23@users.noreply.github.com>
2023-10-01 18:01:57 -0400

    added coding exercise 3 changes

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -3,8 +3,9 @@ import React from  'react';
 function AboutPage() {
   return (
     <div className="App">
-        AboutPage
-    </div>
+    <h2>This is about page of this website</h2>
+    <p>Creating a budget, which is a detailed plan outlining your income, expenses, and savings, is the cornerstone of effective budget management. Tracking your expenses, differentiating between essential needs and discretionary wants, and regularly reviewing and adjusting your budget are essential components. </p>
+    <p>Thank you</p>    </div>
   );
 }
 
--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,9 +1,26 @@
-import React from  'react';
+import React from 'react';
 
 function LoginPage() {
   return (
     <div className="App">
-        LoginPage
+      <h2>Login</h2>
+      <form action="login_process.php" method="post">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Sign In" />
+        </div>
+      </form>
+      <div className="options">
+        <a href="signup.html">Sign Up</a> |
+        <a href="forgot_password.html">Forgot Password</a>
+      </div>
     </div>
   );
 }
